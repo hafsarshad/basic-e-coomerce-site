b2e0acb31209b24f0e64278334531df1d3baebd1
hafsarshad <hafsarshad123@gmail.com>
2025-06-25 23:08:49 +0500

    tailwind issue remain but backend works properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      ".src/app/**/*.{js,ts,jsx,tsx}",
-      ".src/app/pages/**/*.{js,ts,jsx,tsx}",
-      ".src/components/**/*.{js,ts,jsx,tsx}",
-    ],
-   theme: {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
     extend: {
-     colors: {
-       lightgray: '#F3F3F4',
+      colors: {
+        lightgray: '#F3F3F4',
       },
       fontFamily: {
         instrument: ['"Instrument Sans"', 'sans-serif'],
       },
     },
   },
-    plugins: [],
-  }
+  plugins: [],
+}
+
   
