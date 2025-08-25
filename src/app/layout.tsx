afe0eb3919bae1bd36727cc5b95cc6fd144473ad
hafsarshad <hafsarshad123@gmail.com>
2025-08-25 23:46:03 +0500

    come on basic now start different layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 
-'use client';
+
 import './globals.css';
 import  Navbar from './components/Navbar'; // ✅ correct
 import { type Metadata } from 'next';
 import { ReactNode } from 'react';
 import { CartProvider } from './context/CartContext';
 import { Geist, Geist_Mono } from 'next/font/google';
-import { usePathname } from 'next/navigation';
+//import { usePathname } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Next.js App',
@@ -14,13 +14,13 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  const pathname = usePathname();
+  //const pathname = usePathname();
   const hideNavbarRoutes = ['/auth/login', '/auth/signup'];
-  const showNavbar = !hideNavbarRoutes.includes(pathname);
+  //const showNavbar = !hideNavbarRoutes.includes(pathname);
   return (
     <html lang="en" >
       <body className=" ">
-      {showNavbar && <Navbar />}
+       <Navbar />
       <CartProvider>
           {children}
         </CartProvider>
