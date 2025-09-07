a58c5a555de6e58d499786c8b07a07cca1d515b6
hafsarshad <hafsarshad123@gmail.com>
2025-09-07 22:32:08 +0500

    carousel animation making

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 // app/page.tsx
 
 import { redirect } from 'next/navigation';
-
+  // app/page.tsx or app/carousel/page.tsx
+import Carousel from '../app/components/carousel';
 export default function RootPage() {
+  <Carousel />
+
   redirect('/home');
   
 }
