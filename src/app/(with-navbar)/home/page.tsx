cacd99b23bc7194dc5b494c5884cde3322bd4116
hafsarshad <hafsarshad123@gmail.com>
2025-09-21 21:13:17 +0500

    header design done with three carousel.

--- src/app/(with-navbar)/home/page.tsx
+++ src/app/(with-navbar)/home/page.tsx
@@ -8,37 +8,6 @@ import { Product } from '@/types';
 import Header from '../../components/header'
 
 export default function HomePage() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState(true);
-  const { addToCart } = useCart();
-  const router = useRouter();
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const res = await fetch('/api/products');
-        const data = await res.json();
-        setProducts(data);
-      } catch (error) {
-        console.error('Failed to fetch products:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchProducts();
-  }, []);
-
-  const handleAddToCart = (product: Product) => {
-    const userId = sessionStorage.getItem('userId');
-    if (!userId) {
-      alert('You must sign up first.');
-      router.push('/auth/signup');
-      return;
-    }
-
-    addToCart(product);
-    router.push('/checkout');
-  };
 
   return (
 
