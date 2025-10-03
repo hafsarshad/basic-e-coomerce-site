4dd0c05b3a20cac2214c77f07c35949ed718135a
hafsarshad <hafsarshad123@gmail.com>
2025-10-03 22:04:33 +0500

    images

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,41 +4,30 @@ import Link from 'next/link';
 import Carousel from '../components/carousel';
 export default function header(){
   const carousel1Images = [
-  '/images/airpodswhite.png',
-   '/images/hairdryer.png',
-   '/images/smartwatch.png',
-    '/images/electrictrimmer.png',
+  // '/images/airpodswhite.png',
+   '/images/personalcare.png',
+   '/images/mobile.png',
+  '/images/homeandkitchen.png'
   //  '/images/bluetoothspeaker.png',
   //  '/images/gamingconsole.png',
 ];
 
-const carousel2Images = [
-  '/images/home.png',
-  '/images/electrickettle.png',
-   '/images/drillmachine.png',
-  // '/images/bigbeater.png',
-  //  '/images/blender.png',
-
-];
-
-const carousel3Images = [
-  '/images/curler.png',
-  '/images/hairdryer.png',
-  '/images/lednaillamp.png',
-  // '/images/electrictrimmer.png',
-];
 
     return(
   <div className='flex  pt-16'>
     <div className=" max-w-screen-md ">
       <h1 className="mt-3 text-logoblack font-albert text-7xl font-medium">Power Up Your World With The Latest Electronics!</h1>
       <h1 className="w-4/5 mt-3 text-logoblack font-albert text-3xl font-normal">our one-stop shop for the latest electronics at unbeatable prices.</h1>
+       <div className="flex gap-6 mt-5">
+          <button className="bg-requiredgreen text-white rounded-3xl px-3 py-2 ">Buy less than 5$</button>
+          <button className="bg-requiredgreen text-white  rounded-3xl px-3 py-2">Shop Now</button>
+        </div>
     </div>
-<div className="w-[40%] mx-auto ">
-  {/* First Row */}
-  <div className="flex justify-center">
-    <Carousel images={carousel1Images} />
-  </div>
+      <div className="w-[40%] mx-auto ">
+        {/* First Row */}
+        <div className="flex justify-center">
+          <Carousel images={carousel1Images} />
+        </div>
  
 </div>
 </div>
